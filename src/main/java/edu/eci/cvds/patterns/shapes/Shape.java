16baaf473b19e1427db63b5eb52023e9ba3b423f
Julian Cardenas <julian.cardenas-c@mail.escuelaing.edu.co>
2025-01-30 21:39:21 -0500

    Arreglo archivos

--- src/main/java/edu/eci/cvds/patterns/shapes/Shape.java
+++ src/main/java/edu/eci/cvds/patterns/shapes/Shape.java
@@ -1,4 +1,4 @@
-package edu.eci.cvds.patterns.shapes;
+package edu.eci.cvds.patterns.archetype.shapes;
 
 public interface Shape {
     public int getNumberOfEdges();
--- src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
+++ src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -3,5 +3,5 @@ package edu.eci.cvds.patterns.archetype.shapes;
 import java.awt.*;
 
 public class ShapeFactory {
-    
+
 }
